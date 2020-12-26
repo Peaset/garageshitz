87d9988ba3a5261683d460ed1eff160fdcae2b69
Peaset <76628978+Peaset@users.noreply.github.com>
2020-12-26 23:38:49 +0300

    Add files via upload

---
+++ garageshitz/config.lua
@@ -0,0 +1,482 @@
+Config = {}
+
+Config.VehicleMenu = true -- enable this if you wan't a vehicle menu.
+Config.VehicleMenuButton = 344 -- change this to the key you want to open the menu with. buttons: https://docs.fivem.net/game-references/controls/
+Config.RangeCheck = 25.0 -- this is the change you will be able to control the vehicle.
+
+Config.Garages = {
+    ["A"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(211.8094329834, -934.98626708984, 24.275938034058)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(219.86030578613, -932.77551269531, 24.141599655151), 
+                ["heading"] = 140.0
+            }
+        },
+        ["camera"] = {  -- camera is not needed just if you want cool effects.
+            ["x"] = 224.94281005859, 
+            ["y"] = -930.33062744141, 
+            ["z"] = 26.571212768555, 
+            ["rotationX"] = -31.401574149728, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = -243.40157422423 
+        }
+    },
+
+    ["B"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(273.67422485352, -344.15573120117, 44.919834136963)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(272.50082397461, -337.40579223633, 44.919834136963), 
+                ["heading"] = 160.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 283.28225708008, 
+            ["y"] = -333.24017333984, 
+            ["z"] = 50.004745483398, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["C"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-1803.8967285156, -341.45928955078, 43.986347198486)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-1810.7857666016, -337.13592529297, 43.552074432373), 
+                ["heading"] = 320.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -1813.5513916016, 
+            ["y"] = -340.40087890625, 
+            ["z"] = 46.962894439697, 
+            ["rotationX"] = -39.496062710881, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = -42.110235854983 
+        }
+    },
+
+    ["D"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(218.18, -761.31, 30.85)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(220.98, -773.09, 30.8), 
+                ["heading"] = 160.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 235.74, 
+            ["y"] = -760.89, 
+            ["z"] = 43.46, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["E"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-109.87, -1034.38, 27.27)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-99.62, -1024.59, 27.27), 
+                ["heading"] = 160.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -77.3, 
+            ["y"] = -1011.4, 
+            ["z"] = 37.23, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["F"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(364.08, 296.44, 103.49)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(362.6, 279.02, 103.27), 
+                ["heading"] = 229.32
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 380.03, 
+            ["y"] = 288.7, 
+            ["z"] = 112.41, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["G"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-55.13, -215.3, 45.44)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-70.25, -213.43, 45.44), 
+                ["heading"] = 156.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -48.12, 
+            ["y"] = -210.1, 
+            ["z"] = 55.0, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["H"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-1076.95, -1268.07, 5.87)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-1075.33, -1245.45, 37), 
+                ["heading"] = 215.77
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -1060.79, 
+            ["y"] = -1237.11, 
+            ["z"] = 14.33, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["I"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-1172.93, -873.46, 14.18)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-1169.8, -892.28, 13.96), 
+                ["heading"] = 34.52
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -1158.43, 
+            ["y"] = -884.46, 
+            ["z"] = 18.31, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["J"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(965.86, -1014.91, 40.85)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(974.34, -1025.95, 41.05), 
+                ["heading"] = 266.13
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 988.48, 
+            ["y"] = -1012.51, 
+            ["z"] = 47.61, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["K"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(480.38, -1962.48, 24.66)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(481.91, -1981.76, 24.73), 
+                ["heading"] = 215.77
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 495.47, 
+            ["y"] = -1964.44, 
+            ["z"] = 31.44, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["L"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-743.06, -78.65, 41.75)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-743.2, -64.34, 41.75), 
+                ["heading"] = 114.95
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -725.03, 
+            ["y"] = -55.07, 
+            ["z"] = 50.44, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["M"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-474.25, -624.81, 31.17)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-493.26,-602.44, 31.17), 
+                ["heading"] = 270.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -473.08, 
+            ["y"] = -589.12, 
+            ["z"] = 40.35, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["N"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-1618.92, -871.81, 9.61)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-1640.86, -870.68, 9.18), 
+                ["heading"] = 45.02
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -1627.97, 
+            ["y"] = -860.11, 
+            ["z"] = 15.91, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["O"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(553.5, -135.07, 59.55)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(545.86, -142.83, 58.65), 
+                ["heading"] = 88.09
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 553.67, 
+            ["y"] = -129.78, 
+            ["z"] = 65.17, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["P"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-1523.92, -420.93, 35.44)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-1525.51, -436.77, 35.44), 
+                ["heading"] = 212.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -1502.16, 
+            ["y"] = -417.58, 
+            ["z"] = 44.72, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["R"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-3155.94, 1089.22, 20.71)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-3143.26, 1095.41, 20.69), 
+                ["heading"] = 321.92
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -3133.81, 
+            ["y"] = 1108.2, 
+            ["z"] = 28.62, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["S"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(380.4, 2635.35, 44.5)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(354.1, 2632.89, 44.5), 
+                ["heading"] = 105.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 359.53, 
+            ["y"] = 2646.84, 
+            ["z"] = 54.45, 
+            ["rotationX"] = -30.637795701623, 
+            ["rotationY"] = 20.0, 
+            ["rotationZ"] = 125.73228356242 
+        }
+    },
+
+    ["T"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(-1501.39, 1505.09, 115.46)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(-1494.06, 1524.73, 114.77), 
+                ["heading"] = 114.0
+            }
+        },
+        ["camera"] = { 
+            ["x"] = -1466.48, 
+            ["y"] = 1540.95, 
+            ["z"] = 133.47, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242  
+        }
+    },
+
+    ["U"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(1741.5, 3711.42, 34.16)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(1736.98, 3720.85, 34.02), 
+                ["heading"] = 23.61
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 1751.41, 
+            ["y"] = 3732.16, 
+            ["z"] = 41.77, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242  
+        }
+    },
+
+    ["V"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(1719.93, 4799.02, 41.75)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(1690.46, 4804.76, 41.83), 
+                ["heading"] = 175.25
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 1712.51, 
+            ["y"] = 4812.65, 
+            ["z"] = 49.97, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242  
+        }
+    },
+
+    ["Y"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(2782.57, 3493.4, 55.19),
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(2792.95, 3498.99, 55.05), 
+                ["heading"] = 154.77
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 2805.67, 
+            ["y"] = 3506.29, 
+            ["z"] = 63.13, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242  
+        }
+    },
+
+    ["Z"] = {
+        ["positions"] = {
+            ["menu"] = {
+                ["position"] = vector3(139.39, 6575.39, 32.01)
+            },
+            ["vehicle"] = {
+                ["position"] = vector3(161.79, 6575.11, 31.83), 
+                ["heading"] = 213.6
+            }
+        },
+        ["camera"] = { 
+            ["x"] = 180.99, 
+            ["y"] = 6588.42, 
+            ["z"] = 38.27, 
+            ["rotationX"] = -21.637795701623, 
+            ["rotationY"] = 0.0, 
+            ["rotationZ"] = 125.73228356242  
+        }
+    },
+}
+
+Config.Labels = {
+    ["menu"] = "~INPUT_CONTEXT~ Garajı acmak icin basiniz. %s.",
+    ["vehicle"] = "'%s'ı koymak icin ~INPUT_CONTEXT~ Tusuna  basiniz."
+}
+
+Config.Trim = function(value)
+	if value then
+		return (string.gsub(value, "^%s*(.-)%s*$", "%1"))
+	else
+		return nil
+	end
+end
+
+Config.AlignMenu = "right" -- this is where the menu is located [left, right, center, top-right, top-left etc.]
